b4cf63d1cf519fd78a6f0d60fc6e5d9ed2f88894
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-12-05 14:57:35 -0500

    kmeiklejohn

--- anagrams.py
+++ anagrams.py
@@ -39,11 +39,13 @@ def find_anagrams(words):
         {'dgo': ['dog'], 'act': ['cat', 'act']}
 
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        a_word = alphabetize(word)
+        if a_word not in anagrams:
+            anagrams[a_word] = [word]
+        else:
+            anagrams[a_word].append(word)
     return anagrams
 
 
